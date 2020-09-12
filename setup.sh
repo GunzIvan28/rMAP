78e553952adbab85be14c4a747fb65c17672b282
GunzIvan28 <32091245+GunzIvan28@users.noreply.github.com>
2020-09-12 13:32:18 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ chmod 777 ../miniconda3/envs/rMAP-1.0/bin/rMAP
 echo -e "rMAP is all set up!!! Run 'rMAP -h' to confirm."
 touch ../clean.sh &
 echo "#! /usr/bin/env bash" >../clean.sh
-echo "rm -rf original-rMAP" >>../clean.sh
+echo "rm -rf rMAP" >>../clean.sh
 chmod 777 ../clean.sh
 cd
 bash clean.sh
